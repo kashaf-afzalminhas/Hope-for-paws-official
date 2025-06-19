593168b952c33326eae9fea6f29d52d83b8fe329
Sahab Noor <sahabnoor193@gmail.com>
2025-06-19 20:30:45 +0500

    Contact Us updated

--- hope-for-paws/src/config.js
+++ hope-for-paws/src/config.js
@@ -1,2 +1,5 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://hope-for-paws-official-backend.vercel.app/api';
-export const AUTH_BASE_URL = `${API_BASE_URL.replace('/api', '')}/auth`; 
+// export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://hope-for-paws-official-backend.vercel.app/api';
+ 
+
+export const API_BASE_URL = 'http://localhost:3000/api';
+export const AUTH_BASE_URL = `${API_BASE_URL.replace('/api', '')}/auth`;
