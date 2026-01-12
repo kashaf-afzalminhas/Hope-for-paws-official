2e2280b7945abb67c482beb07a2912430f21edc5
kashaf-afzalminhas <kashafafzal909@gmail.com>
2026-01-12 10:25:30 +0500

    mongoose connection error fixed

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,10 @@ const NotificationService = require('./services/notificationService');
 
 dotenv.config();
 console.log('MONGO_URI:', process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, {
+  tls: true,
+  serverSelectionTimeoutMS: 30000,
+})
   .then(() => {
     console.log('MongoDB connected successfully');
   })
